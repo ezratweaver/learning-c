a9f3970cb103a5bcda1dffd390003c3d3f9af1f6
ezratweaver <ezratweaver@gmail.com>
2023-07-05 15:12:55 -0500

    Format stuff

---
+++ prices.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    float apple = 2.99;
+    float orange = 0.99;
+    float banana = 0.50;
+
+    printf("Apple Price: $%.2f\n", apple);
+    printf("Orange Price: $%.2f\n", orange);
+    printf("Banana Price: $%.2f\n", banana);
+
+}
