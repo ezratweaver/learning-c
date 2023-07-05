18463ffb179f76d8c9d8a599731f81ad92f9c345
ezratweaver <ezratweaver@gmail.com>
2023-07-04 23:29:25 -0500

    Messing With Variables

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char name[] = "Ezra";
+char grade = 'C';
+int age = 17;
+float gpa = 4.0;
+
+int main(void){
+
+    printf("Your Name is %s\n", name);
+    printf("Your Grade is %c\n", grade);
+    printf("Your Age is %d\n", age);
+    printf("Your GPA is %f", gpa);
+
+}
